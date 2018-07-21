1f4ddfe21d76684997799b1018c7665a4f257831
Jan Samuel Owczarek <jan.samuel.o@gmail.com>
2018-07-21 22:48:56 +0200

    whole project reconstructed

--- game.cpp
+++ game.cpp
@@ -1,46 +1,120 @@
 #include "game.h"
-#include <iostream>
-#include <vector>
+
+std::unique_ptr<Monster> Game::newcreature(std::string name, int x, int y)
+{
+    //for addition of new monster add to this list
+    // else if(name==""){return new Creature tmp(x,y,name,M,health points,damage,gold on monster,this)}
+
+    if(name=="rat"){return std::unique_ptr<Monster>(new Monster(x,y,name,'M',5,1,5,this));}
+    else if(name=="bat"){return std::unique_ptr<Monster>(new Monster(x,y,name,'M',2,3,1,this));}
+    else if(name=="orc"){return std::unique_ptr<Monster>(new Monster(x,y,name,'M',10,1,5,this));}
+    else if(name=="troll"){return std::unique_ptr<Monster>(new Monster(x,y,name,'M',10,4,8,this));}
+    else return 0;
+}
+void Game::monster_maker(std::string name,int x,int y)
+{
+    std::unique_ptr<Monster>newMonster=newcreature(name,x,y);
+    monster_list.push_back(std::move(newMonster));
+}
+void Game::monster_maker(std::string name,int x, int y,char symbol,int health_points,int damage,int gold)
+{
+    std::unique_ptr<Monster> newMonster=std::unique_ptr<Monster>(new Monster(x,y,name,symbol,health_points,damage,gold,this));
+    monster_list.push_back(std::move(newMonster));
+}
+void Game::wall_maker(int x,int y)
+{
+    std::unique_ptr<Wall> newWall=std::unique_ptr<Wall>(new Wall(x,y,this));
+    wall_list.push_back(std::move(newWall));
+}
+void Game::wall_maker(int x1,int y1,int x2, int y2)
+{
+    if(x1==x2)
+    {
+        if(y1>=y2)
+        {
+            for(int i=y2;i<=y1;++i)
+            {
+                std::unique_ptr<Wall> newWall=std::unique_ptr<Wall>(new Wall(x1,i,this));
+                wall_list.push_back(std::move(newWall));            }
+        }
+        else
+        {
+            for(int i=y1;i<=y2;++i)
+            {
+                std::unique_ptr<Wall> newWall=std::unique_ptr<Wall>(new Wall(x1,i,this));
+                wall_list.push_back(std::move(newWall));            }
+        }
+    }
+    else if(y1==y2)
+    {
+        if(x1>=x2)
+        {
+            for(int i=x2;i<=x1;++i)
+            {
+                std::unique_ptr<Wall> newWall=std::unique_ptr<Wall>(new Wall(i,y1,this));
+                wall_list.push_back(std::move(newWall));            }
+        }
+        else
+        {
+            for(int i=x1;i<=x2;++i)
+            {
+                std::unique_ptr<Wall> newWall=std::unique_ptr<Wall>(new Wall(i,y1,this));
+                wall_list.push_back(std::move(newWall));            }
+        }
+    }
+    else
+        std::cout<<"Error: wall maker"<<std::endl;
+}
+void Game::MoveObj_maker(int x,int y)
+{
+    std::unique_ptr<Move_Object> newObj=std::unique_ptr<Move_Object>(new Move_Object(x,y,this));
+    object_list.push_back(std::move(newObj));
+}
+void Game::TextObj_maker(int x,int y,std::string text)
+{
+    std::unique_ptr<Text_Object> newObj=std::unique_ptr<Text_Object>(new Text_Object(x,y,text,this));
+    object_list.push_back(std::move(newObj));
+}
 
 void Game::set_map()//loading objects on map
 {
-    size_x=10;
-    size_y=10;
+    size_x=150;
+    size_y=200;
     _map.resize(size_x, std::vector<Field>(size_y));
-    Creature(5,5,"monster",'M',5,1,5,this);
-    Wall w01(0,0,this);
-    Wall w02(0,1,this);
-    Wall w03(0,2,this);
-    Wall w04(0,3,this);
-    Wall w05(0,4,this);
-    Wall w06(0,5,this);
-    Wall w07(0,6,this);
-    Wall w08(0,7,this);
-    Wall w09(0,8,this);
-    Wall w10(0,9,this);
-    Wall w91(9,0,this);
-    Wall w92(9,1,this);
-    Wall w93(9,2,this);
-    Wall w94(9,3,this);
-    Wall w95(9,4,this);
-    Wall w96(9,5,this);
-    Wall w97(9,6,this);
-    Wall w98(9,7,this);
-    Wall w99(9,8,this);
-    Wall w910(9,9,this);
+    monster_maker("bat",5,5);
+    MoveObj_maker(8,5);
+    TextObj_maker(10,11,"sample text");
+    wall_maker(0,0,0,199);
+    wall_maker(1,0,148,0);
+    wall_maker(3,2,3,50);
+
+
 }
-void Game::map_print()
+void Game::map_print(Player *one)
 {
-    for (int i=0;i<10;++i)
+    const int view_y=8;
+    const int view_x=view_y*2;
+    int y_max,y_min;
+    int x_max,x_min;
     {
-        for(int j=0;j<10;++j)
+        if(one->get_x()<=view_x){x_min=0;}
+        if(one->get_y()<=view_y){y_min=0;}
+        if(one->get_x()>=view_x){x_min=one->get_x()-view_x;}
+        if(one->get_y()>=view_y){y_min=one->get_y()-view_y;}
+        if(one->get_x()+view_x>size_x){x_max=size_x;}
+        if(one->get_y()+view_y>size_y){y_max=size_y;}
+        if(one->get_x()+view_x<size_x){x_max=one->get_x()+view_x;}
+        if(one->get_y()+view_y<size_y){y_max=one->get_y()+view_y;}
+    }
+    for (int i=y_max-1;i>=y_min;--i)
+    {
+        for(int j=x_min;j<x_max;++j)
         {
             std::cout<<_map[j][i].symbol;
         }
         std::cout<<std::endl;
     }
 }
-
 void Game::start()
 {
     std::string name;
@@ -49,16 +123,21 @@ void Game::start()
     std::cin>>name;
     Player one(name,this);
     clear();
+    narration("You are in compleat darknes, no sound, no smell, nothing you can fell, you are part of darkness as it is part of you. You don't feel passage of time almost like even time dosent exist here. With no thouge at all you are suspended as part of this plane. You almoste don't exist");
+    narration("Sparkel. Singel sparkel inside you. You can fell it. Evrything comes back to you, you can fell.\nYou fell coldness and felling of loss. But you can not rember what was lost/nYou look at your hands only to see your white bare bones. So I really died You thouge. ther whern't any meat on your animeted bones. You had died long ago ");
     play( &one);
 }
 void Game::play(Player *one)
 {
     while(1)//finnish()==0)creat fininsh function checking if player finnish stage
     {
-        map_print();
-        one->action(this);
+        map_print(one);
+        one->action();
         clear();
     }
+}
+Game::~Game()
+{
 }
 void Field::zero()
 {
@@ -69,14 +148,39 @@ Field::Field()
 {
     zero();
 }
-
-void Field::set(char x, Over_object *pointer)
+void Field::set(char x, Over_object &pointer)
 {
     symbol=x;
     addres=&pointer;
 }
-
 void clear()
 {
     printf("\e[1;1H\e[2J");
+}
+char getch()
+{
+    char buf = 0;
+    struct termios old = {0};
+    if (tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if (read(0, &buf, 1) < 0)
+        perror ("read()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if (tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror ("tcsetattr ~ICANON");
+    return (buf);
+}
+void narration(std::string text)
+{
+    clear();
+    std::cout<<text<<std::endl;
+    std::cin.get();
+    std::cin.ignore();
 }
--- game.h
+++ game.h
@@ -5,32 +5,68 @@
 //for now you can only move so enjoy
 
 #include <iostream>
-#include <string>
-#include <stdio.h>
 #include <vector>
+#include <unistd.h>
+#include <termios.h>
+#include <memory>
 
 class Player;
+class Monster;
 class Game;
+class Wall;
 
-struct Over_object//every object on map must inherit after this object
+class Over_object//every object on map must inherit after this object
 {
+private:
     char symbol;
     int x;
     int y;
-    Over_object(int,int,char,Game *);
+    Game * board;
+public:
+    Over_object(int,int,char,Game &);
     int get_x()const;
     int get_y()const;
+    char get_symbol()const;
+    Game* board_ptr()const;
     void set_x(int);
     void set_y(int);
-    virtual void collision(Player *,Game *)=0;//reaction trigered if player walks over specfic field
+    virtual ~Over_object(){}
+    virtual void collision(Player *)=0;//reaction trigered if player walks over specfic field
+};
+
+class Object :public Over_object
+{
+public:
+    Object();
+    Object(const Object &other);
+    Object(int ,int ,Game &);
+    virtual void collision(Player *)=0;
+    virtual ~Object(){}
+};
+
+class Move_Object :public Object
+{
+public:
+    Move_Object(int ,int ,Game *);
+    virtual void collision(Player *);
+    virtual ~Move_Object(){}
+};
+
+class Text_Object :public Object
+{
+    std::string text;
+public:
+    Text_Object(int ,int ,std::string ,Game *);
+    virtual void collision(Player *);
+    virtual ~Text_Object(){}
 };
 
 struct Field//smallest part of map
 {
     char symbol;
-    Over_object **addres;
+    Over_object *addres;
     Field();
-    void set(char x,Over_object *);//seting field
+    void set(char x,Over_object &);//seting field
     void zero();//reseting field to empty state
 private:
     int x,y;
@@ -40,64 +76,80 @@ class Game
 {
     int size_x;
     int size_y;
+    std::vector<std::unique_ptr<Monster> >monster_list;
+    std::vector<std::unique_ptr<Wall> >wall_list;
+    std::vector<std::unique_ptr<Object> >object_list;
 public:
+    ~Game();
     std::vector< std::vector<Field> >_map;
     // bool finnish();
     void set_map();
     void start();
     void play(Player *);
-    void map_print();
+    void map_print(Player *);
+    void monster_maker(std::string,int,int);
+    void monster_maker(std::string,int,int,char,int,int,int);
+    std::unique_ptr<Monster> newcreature(std::string,int,int);
+    void wall_maker(int,int);
+    void wall_maker(int,int,int,int);
+    void MoveObj_maker(int x,int y);
+    void TextObj_maker(int x,int y,std::string text);
+
 };
 
 class Wall :public Over_object
 {
 public:
     Wall(int ,int ,Game *);
-    virtual void collision(Player *,Game *);
+    virtual void collision(Player *);
+    virtual ~Wall(){}
 };
 
 class Creature :public Over_object
 {
+protected:
     std::string name;
     int health_points;
     int dmg;
     int gold;
 public:
+    virtual ~Creature(){}
     Creature();
-    Creature(int,int,std::string,char, int, int, int,Game *);
+    Creature(int,int,std::string,char, int, int, int,Game &);
+    Creature(const Creature &other);
     int get_gold()const;
     int get_dmg()const;
     int get_health()const;
-    char get_symbol()const;
     std::string get_name()const;
-    bool isDead();
-    void reduceHealth(int);
-    void kill(Game *);
-    virtual void collision(Player *,Game *);
+    virtual bool isDead();
+    virtual void reduceHealth(int);
+    virtual void collision(Player *)=0;
 };
 
-class Player :public Over_object//player starts on position 2,2
+class Monster :public Creature
 {
-    std::string name;
-    int health_points;
-    int dmg;
-    int gold;
+public:
+    virtual ~Monster(){}
+    void kill();
+    Monster(int x,int y,std::string name,char symbol, int h_point, int damage, int money,Game *game_ptr);
+    virtual void collision(Player *);
+};
+
+class Player :public Creature//player starts on position 2,2
+{
     int level;
 public:
-    Player(std::string,Game *);
-    void action(Game *);
+    virtual ~Player(){}
+    Player(std::string,Game *);  
+    void action();
     int get_leve()const;
     void add_level();
-    void reduceHealth(int);
     void addGold(int);
     void addDMG();
-    std::string get_name()const;
-    int get_gold()const;
-    int get_dmg()const;
-    int get_health()const;
-    char get_symbol()const;
-    void figth (Creature *);
-    virtual void collision(Player *,Game *);
+    void figth (Monster *);
+    virtual void collision(Player *);
 };
 
 void clear();
+char getch();
+void narration(std::string text);
